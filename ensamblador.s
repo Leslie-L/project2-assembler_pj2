0ffd096e8199daea0c4bea396f5f935286ca2067
volant360 <efrainh12@galileo.edu>
2017-02-28 23:29:02 -0600

    despliegue de instrucciones agregado

--- ensamblador.s
+++ ensamblador.s
@@ -7,6 +7,7 @@
 .data
 READ_MODE: .asciz "r"
 HOPE: .asciz "TENGO LA ESPERANZA DE QUE AQUI SALGA LO QUE ESPERO %s\n"
+INSTRUCCION_: .asciz "%x\n"
 args_error_msg: .asciz "No se ingresaron el numero correcto de parametros"
 dot_data: .space 1000 	//el area de data
 dot_text: .space 4000	//el area de texto
@@ -43,6 +44,23 @@ read_file_finish:
    ADD SP, SP, #32
    RET
 
+display_instructions:
+   ADD SP, SP, #-16
+   STR x30,[SP,#0]
+   STR x19,[SP,#8]
+   MOV x19, x0
+display_loop:
+   LDR x0,=INSTRUCCION_
+   LDR w1,[x19,#0]
+   CMP w1,#0
+   B.EQ finish_display
+   BL printf
+   B display_loop
+finish_display:
+   LDR x19,[SP,#8]
+   LDR x30,[SP,#0]
+   ADD SP, SP, #16
+
 encode:
    RET
 
@@ -68,6 +86,8 @@ main:
    MOV x1, x20
    MOV x2, x21
    BL encode		//esta es la ultima instruccion que hay que codificar
+   MOV x0, x20    //desplegaremos las instrucciones codificadas
+   BL display_instructions
 main_finish:
    MOV x0,#0 		// return 0 :)
    LDR x30,[SP,#0]
